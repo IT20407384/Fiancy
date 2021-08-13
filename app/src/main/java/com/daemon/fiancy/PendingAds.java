7074309fcc6f6ea2bd4e4ec1e10e51337b34e27a
ChamodiSankalpani <chamodisankalpani@gmail.com>
2021-08-14 00:19:28 +0530

    Test chamodi 1

---
+++ app/src/main/java/com/daemon/fiancy/PendingAds.java
@@ -0,0 +1,14 @@
+package com.daemon.fiancy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PendingAds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pending_ads);
+    }
+}

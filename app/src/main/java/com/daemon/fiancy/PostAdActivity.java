e0e44afda63a20ce775488001d82464e8b0df07c
chamod1122 <chamodishankha1122@gmail.com>
2021-08-14 02:14:08 +0530

    post ad update

--- app/src/main/java/com/daemon/fiancy/PostAdActivity.java
+++ app/src/main/java/com/daemon/fiancy/PostAdActivity.java
@@ -2,13 +2,20 @@ package com.daemon.fiancy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class PostAdActivity extends AppCompatActivity {
+
+    ImageView back_img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_ad);
+
+        back_img = findViewById(R.id.back);
     }
 }

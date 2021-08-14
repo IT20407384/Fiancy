e5eadd4bff59586818e26a5091aa160a74a08e5d
Suvin <it20407384@my.sliit.lk>
2021-08-15 01:19:46 +0530

    my advertisement, favorites and premium view update.

--- app/src/main/java/com/daemon/fiancy/MyAdCollection.java
+++ app/src/main/java/com/daemon/fiancy/MyAdCollection.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TabHost;
 
-public class PremiumLog extends AppCompatActivity {
+public class MyAdCollection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_premiumlog);
+        setContentView(R.layout.activity_my_ad_collection);
 
         // initiating the tabhost
         TabHost tabhost = (TabHost) findViewById(R.id.tabhost);
@@ -19,13 +19,21 @@ public class PremiumLog extends AppCompatActivity {
         tabhost.setup();
 
         // Code for adding Tab 1 to the tabhost
-        TabHost.TabSpec spec = tabhost.newTabSpec("Fiancy Premium");
-        spec.setContent(R.id.premium);
+        TabHost.TabSpec spec = tabhost.newTabSpec("My Advertisement");
+        spec.setContent(R.id.myadvertisement);
 
         // setting the name of the tab 1 as "Tab One"
-        spec.setIndicator("Fiancy Premium");
+        spec.setIndicator("My Advertisement");
 
         // adding the tab to tabhost
+        tabhost.addTab(spec);
+
+        // Code for adding Tab 2 to the tabhost
+        spec = tabhost.newTabSpec("My Favorites");
+        spec.setContent(R.id.myfavorites);
+
+        // setting the name of the tab 1 as "Tab Two"
+        spec.setIndicator("My Favorites");
         tabhost.addTab(spec);
     }
 }

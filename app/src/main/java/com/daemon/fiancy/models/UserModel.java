9f9d68618eb13ff43e3d916f21a75f4bdf90ab73
Suvin <it20407384@my.sliit.lk>
2021-09-18 20:19:26 +0530

    Model package added, company logo updated

---
+++ app/src/main/java/com/daemon/fiancy/models/UserModel.java
@@ -0,0 +1,44 @@
+package com.daemon.fiancy.models;
+
+public class UserModel {
+
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private String password;
+
+    public UserModel() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

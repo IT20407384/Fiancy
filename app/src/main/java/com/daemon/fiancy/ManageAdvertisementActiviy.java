5f9f9f1148e4055630a81b5094251a68a9da0378
chamod1122 <chamodishankha1122@gmail.com>
2021-09-22 11:53:46 +0530

    adconfirm button hide fix in adconfirmation activity.

--- app/src/main/java/com/daemon/fiancy/ManageAdvertisementActiviy.java
+++ app/src/main/java/com/daemon/fiancy/ManageAdvertisementActiviy.java
@@ -6,16 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class ManageAdvertisementActiviy extends AppCompatActivity {
     String TAG = "Fiancy manage advertisement    :   ";
+    // initialization
+    CheckBox cbReading, cbCollecting, cbMusic, cbGardening, cbGames, cbFishing,
+            cbWalking, cbShopping, cbTraveling, cbWatchingSports, cbEatingOut, cbDancing;
+    RadioGroup radioGroupStatus;
+    RadioButton statusRadioBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_advertisement);
 
-        Log.d("lifecycle", "Application is created");
+        Log.d(TAG, "Application is created");
 
     }
 

4cfb1f42ff87c4754dde0a16ae46ae55add01fda
IT20911744 <IT20911744@my.sliit.lk>
2021-09-26 14:16:56 +0530

    commit test cases

---
+++ app/src/test/java/com/daemon/fiancy/ReasonTest.java
@@ -0,0 +1,29 @@
+package com.daemon.fiancy;
+
+import com.daemon.fiancy.models.RejectedAds;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ReasonTest {
+    private RejectedAds rejectedAds;
+
+    @BeforeEach
+    public void setUp() {
+        rejectedAds = new RejectedAds();
+    }
+
+    @Test
+    public void testMatchCalculation() {
+        int x=0;
+        boolean result = rejectedAds.ReasonNull("Chamodi");
+        if(result){
+            x=1;
+        }
+        assertEquals(1,x);
+    }
+
+}
